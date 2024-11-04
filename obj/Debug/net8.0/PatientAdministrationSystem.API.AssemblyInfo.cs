b98cb60cddd528c778459abe6da1c89dfdac13c5
PAULINE O CONNOR <96267933+Boulaower@users.noreply.github.com>
2024-11-04 22:06:02 +0000

    Initial commit

--- obj/Debug/net8.0/PatientAdministrationSystem.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/PatientAdministrationSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientAdministrationSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0425aadd425d503cc00223d637312ff1890c7b96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2e4dcd24e61cb05d8d0747121069c79677e324")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientAdministrationSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientAdministrationSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
